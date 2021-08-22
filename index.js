c2148bdf6be8371183190d260a3f1e79f854943f
Jose Avalos <avaljose@hotmai.com>
2021-08-22 12:03:37 -0500

    finished employee test

---
+++ index.js
@@ -0,0 +1,6 @@
+const Inquierer = require("Inquierer");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
