60f1630f34050570ade523711b3c70be7a52ec32
Jose Avalos <avaljose@hotmai.com>
2021-08-25 22:24:37 -0500

    adding switch for roles

--- index.js
+++ index.js
@@ -1,6 +1,54 @@
-const Inquierer = require("Inquierer");
+const inquirer = require("inquirer");
 const Employee = require("./lib/Employee");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
 const Manager = require("./lib/Manager");
+const fs = require('fs')
+//arrays
+const employeeArray = [];
+const engineerArray = [];
+const internArray = [];
+const managerArray = [];
 
+function initApp() {
+    startHtml();
+    addEmployees();
+}
+
+function addEmployees() {
+    inquirer.prompt([{
+        type: "input",
+        Message: "Enter team memeber's name",
+        name: "name",
+    },
+    {
+        type: "list",
+        Message: "Select team member's role",
+        choices: ["Engineer", 'Intern', 'Manager'],
+        name: 'role'
+    },
+    {
+        type: "input",
+        Message: "Enter team memeber's id",
+        name: "id",
+    },
+    {
+        type: "input",
+        Message: "Enter team memeber's email",
+        name: "email",
+    },
+])}
+.then(function({name, role, id, email}){
+    let roleInfo= '';
+    switch (role) {
+        case 'Engineer': {
+            roleInfo = 'Github username';
+        break;}
+        case 'Intern': {
+            roleInfo = 'School name';
+        break;}
+        case 'Manager': {
+            roleInfo = 'Office phone number';
+        break;}
+    }
+})
